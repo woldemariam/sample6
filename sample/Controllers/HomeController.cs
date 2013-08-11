6e108f4b3f752d2e49929bc44cd8dea8978c38d8
woldemariam <woldemariam@gmail.com>
2013-08-10 21:43:17 -0400

    f

---
+++ sample/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using sample.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            //using (Entities e = new Entities())
+            //{
+            //    var item = e.Englishes.First();
+            //    ViewBag.Message = item.Term;
+
+            //}
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
